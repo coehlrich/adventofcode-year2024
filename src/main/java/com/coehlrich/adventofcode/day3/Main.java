29f459de37d838d5a7162ee040d1f6f815269bc8
coehlrich <coehlrich@users.noreply.github.com>
2024-12-03 18:09:13 +1300

    Add day 3

---
+++ src/main/java/com/coehlrich/adventofcode/day3/Main.java
@@ -0,0 +1,35 @@
+package com.coehlrich.adventofcode.day3;
+
+import com.coehlrich.adventofcode.Day;
+import com.coehlrich.adventofcode.Result;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main implements Day {
+
+    private static final Pattern MUL = Pattern.compile("do\\(\\)|don't\\(\\)|mul\\((\\d{1,3}),(\\d{1,3})\\)");
+
+    @Override
+    public Result execute(String input) {
+        int part1 = 0;
+        int part2 = 0;
+        Matcher matcher = MUL.matcher(input);
+        boolean enable = true;
+        while (matcher.find()) {
+            switch (matcher.group(0).replaceAll("\\(.*\\)", "")) {
+                case "do" -> enable = true;
+                case "don't" -> enable = false;
+                case "mul" -> {
+                    int result = Integer.parseInt(matcher.group(1)) * Integer.parseInt(matcher.group(2));
+                    part1 += result;
+                    if (enable) {
+                        part2 += result;
+                    }
+                }
+            }
+        }
+        return new Result(part1, part2);
+    }
+
+}
