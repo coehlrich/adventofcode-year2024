c04fcae05fec77f237024341076ae1a321e8f73c
coehlrich <coehlrich@users.noreply.github.com>
2024-12-05 19:10:13 +1300

    Improve performance

--- src/main/java/com/coehlrich/adventofcode/day5/Main.java
+++ src/main/java/com/coehlrich/adventofcode/day5/Main.java
@@ -2,13 +2,11 @@ package com.coehlrich.adventofcode.day5;
 
 import com.coehlrich.adventofcode.Day;
 import com.coehlrich.adventofcode.Result;
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
-import it.unimi.dsi.fastutil.ints.IntList;
+import it.unimi.dsi.fastutil.ints.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -36,22 +34,20 @@ public class Main implements Day {
         int part1 = 0;
         int part2 = 0;
         for (IntList page : pages) {
-            Int2ObjectMap<List<Policy>> invalid = check(page, before);
-            if (invalid.isEmpty()) {
+            if (check(page, before)) {
                 part1 += page.getInt(page.size() / 2);
             } else {
+                Int2ObjectMap<IntList> invalid = new Int2ObjectOpenHashMap<>();
+                for (int value : page) {
+                    invalid.put(value, new IntArrayList(before.get(value).stream()
+                            .mapToInt(Policy::after)
+                            .filter(page::contains)
+                            .toArray()));
+                }
                 page = new IntArrayList(page);
-                while (!invalid.isEmpty()) {
-                    for (Int2ObjectMap.Entry<List<Policy>> entry : invalid.int2ObjectEntrySet()) {
-                        int min = entry.getValue().stream()
-                                .mapToInt(Policy::after)
-                                .map(page::indexOf)
-                                .min()
-                                .getAsInt();
-                        int value = page.removeInt(entry.getIntKey());
-                        page.add(min, value);
-                    }
-                    invalid = check(page, before);
+                IntSet sorted = new IntOpenHashSet();
+                for (Int2ObjectMap.Entry<IntList> entry : invalid.int2ObjectEntrySet()) {
+                    sort(page, entry.getIntKey(), entry.getValue(), invalid, sorted);
                 }
                 part2 += page.getInt(page.size() / 2);
             }
@@ -59,20 +55,41 @@ public class Main implements Day {
         return new Result(part1, part2);
     }
 
-    private Int2ObjectMap<List<Policy>> check(IntList page, Int2ObjectMap<List<Policy>> before) {
-        Int2ObjectMap<List<Policy>> invalid = new Int2ObjectOpenHashMap<>();
+    private boolean check(IntList page, Int2ObjectMap<List<Policy>> before) {
         for (int i = 0; i < page.size(); i++) {
             if (before.containsKey(page.getInt(i))) {
                 List<Policy> policies = before.get(page.getInt(i));
                 for (Policy policy : policies) {
                     int afterIndex = page.indexOf(policy.after());
                     if (afterIndex < i && afterIndex != -1) {
-                        invalid.computeIfAbsent(i, i2 -> new ArrayList<>()).add(policy);
+                        return false;
                     }
                 }
             }
         }
-        return invalid;
+        return true;
+    }
+
+    private void sort(IntList page, int key, IntList value, Int2ObjectMap<IntList> invalid, IntSet sorted) {
+        if (sorted.contains(key)) {
+            return;
+        }
+        for (int dependency : value) {
+            if (!sorted.contains(dependency) && invalid.containsKey(dependency)) {
+                sort(page, dependency, invalid.get(dependency), invalid, sorted);
+            }
+        }
+        OptionalInt min = value.intStream()
+                .map(page::indexOf)
+                .min();
+        if (min.isPresent()) {
+            int index = page.indexOf(key);
+            if (index > min.getAsInt()) {
+                page.removeInt(index);
+                page.add(min.getAsInt(), key);
+            }
+        }
+        sorted.add(key);
     }
 
 }
