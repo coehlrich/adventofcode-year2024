799d94b350782da64d05b68e2d74cfe997583a6c
coehlrich <coehlrich@users.noreply.github.com>
2024-12-14 18:56:20 +1300

    Add day 14

---
+++ src/main/java/com/coehlrich/adventofcode/day14/Main.java
@@ -0,0 +1,86 @@
+package com.coehlrich.adventofcode.day14;
+
+import com.coehlrich.adventofcode.Day;
+import com.coehlrich.adventofcode.Result;
+import com.coehlrich.adventofcode.util.Point2;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main implements Day {
+
+    private static final Pattern ROBOT = Pattern.compile("p=(-?\\d+),(-?\\d+) v=(-?\\d+),(-?\\d+)");
+
+    @Override
+    public Result execute(String input) {
+        int width = 101;
+        int height = 103;
+
+        List<Robot> robots = input.lines().map(line -> {
+            Matcher matcher = ROBOT.matcher(line);
+            matcher.matches();
+            Point2 start = new Point2(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)));
+            Point2 velocity = new Point2(Integer.parseInt(matcher.group(3)), Integer.parseInt(matcher.group(4)));
+            return new Robot(start, velocity);
+        }).toList();
+
+        int nw = 0;
+        int ne = 0;
+        int sw = 0;
+        int se = 0;
+
+        int part2 = 0;
+        for (Robot robot : robots) {
+            Point2 end = robot.start().offset(robot.velocity().multiply(100));
+            int x = Math.floorMod(end.x(), width);
+            int y = Math.floorMod(end.y(), height);
+
+            if (x < width / 2 && y < height / 2) {
+                nw++;
+            } else if (x > width / 2 && y < height / 2) {
+                ne++;
+            } else if (x < width / 2 && y > height / 2) {
+                sw++;
+            } else if (x > width / 2 && y > height / 2) {
+                se++;
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < 10000; i++) {
+            boolean[][] map = new boolean[height][width];
+            for (Robot robot : robots) {
+                Point2 end = robot.start().offset(robot.velocity().multiply(i));
+                int x = Math.floorMod(end.x(), width);
+                int y = Math.floorMod(end.y(), height);
+
+                map[y][x] = true;
+            }
+            int amount = 0;
+
+            for (int y = 17; y < 51; y++) {
+                if (map[y][19]) {
+                    amount++;
+                }
+            }
+//            System.out.println(amount);
+            if (amount == 33) {
+                if (part2 == 0) {
+                    part2 = i;
+                }
+//                System.out.println((i + 1) + ":");
+                for (int y = 0; y < map.length; y++) {
+                    for (int x = 0; x < map[0].length; x++) {
+                        System.out.print(map[y][x] ? '#' : '.');
+                    }
+                    System.out.println();
+                }
+            }
+        }
+
+        return new Result(nw * ne * sw * se, part2);
+    }
+
+}
---
+++ src/main/java/com/coehlrich/adventofcode/day14/Robot.java
@@ -0,0 +1,7 @@
+package com.coehlrich.adventofcode.day14;
+
+import com.coehlrich.adventofcode.util.Point2;
+
+public record Robot(Point2 start, Point2 velocity) {
+
+}
