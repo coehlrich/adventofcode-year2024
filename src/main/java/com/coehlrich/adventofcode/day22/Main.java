f6fc13b98f59f0ed22988816b6651b9fd4456035
coehlrich <coehlrich@users.noreply.github.com>
2024-12-22 18:43:23 +1300

    Make part 2 more efficient

--- src/main/java/com/coehlrich/adventofcode/day22/Main.java
+++ src/main/java/com/coehlrich/adventofcode/day22/Main.java
@@ -2,15 +2,12 @@ package com.coehlrich.adventofcode.day22;
 
 import com.coehlrich.adventofcode.Day;
 import com.coehlrich.adventofcode.Result;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
-import it.unimi.dsi.fastutil.ints.IntList;
+import it.unimi.dsi.fastutil.ints.Int2IntMap;
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
 import it.unimi.dsi.fastutil.longs.LongArrayList;
 import it.unimi.dsi.fastutil.longs.LongList;
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
-
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main implements Day {
 
@@ -18,11 +15,11 @@ public class Main implements Day {
     public Result execute(String input) {
         LongList numbers = new LongArrayList(input.lines().mapToLong(Long::parseLong).toArray());
         long part1 = 0;
-        Object2IntMap<IntList> part2 = new Object2IntOpenHashMap<>();
+        Int2IntMap part2 = new Int2IntOpenHashMap();
         for (long number : numbers) {
-            IntList changes = new IntArrayList();
+            int changes = 0;
             long previous = -10;
-            Set<IntList> found = new HashSet<>();
+            IntSet found = new IntOpenHashSet();
             for (int i = 0; i < 2000 * 3; i++) {
                 long newNumber = switch (i % 3) {
                     case 0 -> number * 64;
@@ -34,13 +31,13 @@ public class Main implements Day {
                 number %= 16777216;
                 if (i % 3 == 2) {
                     if (previous != -10) {
-                        int change = (int) (number % 10 - previous);
-                        changes.add(change);
-                        if (changes.size() > 4) {
-                            changes.removeInt(0);
-                            if (found.add(new IntArrayList(changes))) {
-                                part2.put(new IntArrayList(changes), (int) (number % 10) + part2.getInt(changes));
-                            }
+                        int change = (int) (number % 10 - previous) + 50;
+                        if (changes >= 1_00_00_00) {
+                            changes %= 1_00_00_00;
+                        }
+                        changes = changes * 100 + change;
+                        if (changes >= 1_00_00_00 && found.add(changes)) {
+                            part2.put(changes, (int) (number % 10) + part2.get(changes));
                         }
                     }
                     previous = number % 10;
